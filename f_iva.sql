b3cd5a093a12a303f619c190e6fec8cc50dcd2d9
David <92442912+D4vid-H@users.noreply.github.com>
2022-09-01 16:45:50 -0300

    Delete f_iva.sql

--- f_iva.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `iva`(precio DECIMAL) RETURNS decimal(10,0)
-    DETERMINISTIC
-BEGIN
-	DECLARE iva DECIMAL;
-    DECLARE precio_con_iva DECIMAL;
-    SET iva = 1.21 ;
-    SET precio_con_iva = precio * iva;
-RETURN precio_con_iva;
-END
