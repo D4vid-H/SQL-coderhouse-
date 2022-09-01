58f9ec063b87c4ca9dbe1c27ea595cdf254b4300
David <92442912+D4vid-H@users.noreply.github.com>
2022-09-01 16:45:59 -0300

    Delete f_nombreCompleto.sql

--- f_nombreCompleto.sql
+++
@@ -1,9 +0,0 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `f_nombreCompleto`(nombre VARCHAR(50), apellido VARCHAR(50)) RETURNS varchar(200) CHARSET utf8mb4
-    DETERMINISTIC
-BEGIN
-
-	DECLARE nombre_completo VARCHAR(200);
-    SET nombre_completo = CONCAT(nombre, ',', apellido);
-
-RETURN nombre_completo;
-END
